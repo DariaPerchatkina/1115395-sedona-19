617b8a025a6b45f65f3be05a2846af82be50bcce
Дарья <perchatkinadaria@gmail.com>
2020-05-09 02:02:34 +0400

    чинит спрайты, добавляет карту

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,6 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    // "source/img/**",
     "source/js/**",
     "source/*.ico"
   ], {
@@ -111,5 +110,4 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-// gulp.task("build", gulp.series("css", "sprite", "html"))
 gulp.task("start", gulp.series("build", "server"));
